32a3ed77d2755d6dbab982a5d1e9a26d43174f85
Ted Ralphs <ted@lehigh.edu>
2008-07-22 23:06:47 +0000

    Updating version numbers and directory names

--- Alps/src/AlpsMessage.cpp
+++ Alps/src/AlpsMessage.cpp
@@ -61,7 +61,7 @@ static Alps_message us_english[] =
     {ALPS_MSG_HOW, 100, 3, "Process[%d] %s count %d in %s"},
     {ALPS_NODE_COUNT, 104, 3, "Worker[%d] processed %d nodes, %d nodes are left"},
     {ALPS_NODE_MEM_SIZE, 106, 1, "The memory size of a node is about %d bytes"},
-    {ALPS_P_VERSION, 110, 1, "ALPS version 0.95.0 (Parallel, MPI)"},
+    {ALPS_P_VERSION, 110, 1, "ALPS version 1.0 (Parallel, MPI)"},
     {ALPS_PARAMFILE, 120, 1, "Parameter file: %s"},
     {ALPS_PEAK_MEMORY, 125, 1, "Peak memory usage: %.2f M"},
     {ALPS_RAMPUP_HUB, 130, 1, "Hub[%d]'s rampup took %g seconds to process %d nodes. Node pool has %d nodes"},
@@ -109,7 +109,7 @@ static Alps_message us_english[] =
     {ALPS_S_FINAL_DEPTH, 272, 1, "Tree depth: %d"},
     {ALPS_S_FINAL_CPU, 274, 1, "Search CPU time: %.2f seconds"},
     {ALPS_S_FINAL_WALLCLOCK, 278, 1, "Search wall-clock time: %.2f seconds"},
-    {ALPS_S_VERSION, 300, 1, "ALPS version 0.95.0 (Serial)"},
+    {ALPS_S_VERSION, 300, 1, "ALPS version 1.0 (Serial)"},
     {ALPS_DUMMY_END, 999999, 0, ""}
 };
 
