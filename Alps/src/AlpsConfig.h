c24db90b61c194b517d7b6fbc50ec31d8ebb118e
Ted Ralphs <ted@lehigh.edu>
2010-02-03 22:26:21 +0000

    Updating copyright statements

--- Alps/src/AlpsConfig.h
+++ Alps/src/AlpsConfig.h
@@ -1,3 +1,25 @@
+/*===========================================================================*
+ * This file is part of the Abstract Library for Parallel Search (ALPS).     *
+ *                                                                           *
+ * ALPS is distributed under the Common Public License as part of the        *
+ * COIN-OR repository (http://www.coin-or.org).                              *
+ *                                                                           *
+ * Authors:                                                                  *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *                                                                           *
+ * Conceptual Design:                                                        *
+ *                                                                           *
+ *          Yan Xu, Lehigh University                                        *
+ *          Ted Ralphs, Lehigh University                                    *
+ *          Laszlo Ladanyi, IBM T.J. Watson Research Center                  *
+ *          Matthew Saltzman, Clemson University                             *
+ *                                                                           *
+ *                                                                           *
+ * Copyright (C) 2006-2010, Lehigh University, Yan Xu, and Ted Ralphs.       *
+ *===========================================================================*/
+
 /*
  * Include file for the configuration of Alps.
  *
