37ab636dead802c320174cce5e61c31c96aaa3bd
Stefan Vigerske <svigerske@gams.com>
2011-07-02 14:17:06 +0000

    fix version numbers in config_alps_default.h

--- Alps/src/config_alps_default.h
+++ Alps/src/config_alps_default.h
@@ -26,13 +26,13 @@
 /***************************************************************************/
 
 /* Version number of project */
-#undef ALPS_VERSION "trunk"
+#undef ALPS_VERSION        "1.3"
 
 /* Major Version number of project */
-#undef ALPS_VERSION_MAJOR   9999
+#undef ALPS_VERSION_MAJOR      1
 
 /* Minor Version number of project */
-#undef ALPS_VERSION_MINOR   9999
+#undef ALPS_VERSION_MINOR      3
 
 /* Release Version number of project */
 #undef ALPS_VERSION_RELEASE 9999
