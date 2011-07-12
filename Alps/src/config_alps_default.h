140c667a5c7b8b614ff1618a9e1c6344b9d2159f
Ted Ralphs <ted@lehigh.edu>
2011-07-12 23:10:00 +0000

    Fixing small problems with configuration

--- Alps/src/config_alps_default.h
+++ Alps/src/config_alps_default.h
@@ -26,16 +26,16 @@
 /***************************************************************************/
 
 /* Version number of project */
-#undef ALPS_VERSION "trunk"
+#define ALPS_VERSION "trunk"
 
 /* Major Version number of project */
-#undef ALPS_VERSION_MAJOR   9999
+#define ALPS_VERSION_MAJOR   9999
 
 /* Minor Version number of project */
-#undef ALPS_VERSION_MINOR   9999
+#define ALPS_VERSION_MINOR   9999
 
 /* Release Version number of project */
-#undef ALPS_VERSION_RELEASE 9999
+#define ALPS_VERSION_RELEASE 9999
 
 /* Define to 1 if the Mpi package is used */
 /* #define COIN_HAS_MPI 1 */
