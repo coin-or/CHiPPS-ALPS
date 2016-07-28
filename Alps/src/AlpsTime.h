069ffc754fd248e9931e7c3cfb08a89a5aa7528e
Aykut Bulut <aykutblt@gmail.com>
2016-07-28 01:21:44 -0400

    Undefs for SEEK_SET, SEEK_END and SEEK_CUR macros removed. They might be necessary a while ago but not anymore. They create problem when compiling with openmpi. After removal both mpich and openmpi works fine.

--- Alps/src/AlpsTime.h
+++ Alps/src/AlpsTime.h
@@ -25,9 +25,9 @@
 
 //#############################################################################
 
-#undef SEEK_SET
-#undef SEEK_END
-#undef SEEK_CUR
+// #undef SEEK_SET
+// #undef SEEK_END
+// #undef SEEK_CUR
 #include "Alps.h"
 #include "AlpsConfig.h"
 
